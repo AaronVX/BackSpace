ccc5a0879dbb445d1a278d6a5e49fe6d10c43a2c
benlenabat <benlenabat@gmail.com>
2019-02-01 16:13:45 -0600

    added another acivity and button

--- app/src/main/java/com/group8/backspace/BookActivity.java
+++ app/src/main/java/com/group8/backspace/BookActivity.java
@@ -1,13 +1,25 @@
 package com.group8.backspace;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class BookActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book);
+        setContentView(R.layout.activity_main);
+
+        Button btn_detail = (Button) findViewById(R.id.btn_detail);
+        btn_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BookActivity.this, FlightDetailActivity.class));
+            }
+        });
     }
+
 }
