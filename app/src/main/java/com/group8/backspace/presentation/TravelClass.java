4fca828898774bcc3853472f73846cd136dc13de
pylypenm <pylypenm@myumanitoba.ca>
2019-02-09 23:04:15 -0500

    added travel class activity

--- app/src/main/java/com/group8/backspace/presentation/TravelClass.java
+++ app/src/main/java/com/group8/backspace/presentation/TravelClass.java
@@ -5,24 +5,42 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 import com.group8.backspace.R;
 
 public class TravelClass extends AppCompatActivity {
-
-    // not sure what to do here...
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_class);
 
-        Button btn_Purchase = (Button) findViewById(R.id.btn_purchase);
-        btn_Purchase.setOnClickListener(new View.OnClickListener() {
+        Button btn_TravelClassSelected = findViewById(R.id.btn_purchase);
+        btn_TravelClassSelected.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(TravelClass.this, PurchaseTicket.class));
+                if(0!=onRadioButtonClicked(v))
+                {
+                    startActivity(new Intent(TravelClass.this, PurchaseTicket.class));
+                }
             }
         });
+    }
+
+    // probably need to change this...
+    public int onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        if(!((RadioButton) view).isChecked())
+            return 0;
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_Poor:
+                return 1;// Poor class selected
+            case R.id.radio_Rich:
+                return 2;// Rich class selected
+        }
+        return 0;
     }
 }
