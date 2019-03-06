d8f67c0a04c53181ca336eaa53c8892d6be8c45c
Ben Le Nabat <benlenabat@gmail.com>
2019-03-06 15:13:29 -0600

    making changes to planet list

--- app/src/main/java/com/group8/backspace/presentation/PlanetList_and_FlightStatus/PlanetList.java
+++ app/src/main/java/com/group8/backspace/presentation/PlanetList_and_FlightStatus/PlanetList.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import com.group8.backspace.R;
+import com.group8.backspace.logic.AccessPlanets;
+import com.group8.backspace.objects.Location;
+
+import java.util.List;
 
 public class PlanetList extends AppCompatActivity {
     ListView simpleList;
@@ -12,13 +16,25 @@ public class PlanetList extends AppCompatActivity {
     String Description[] = {"A warm blue and green planet", "A hot hell hole filled with volcanoes and sulphuric acid", "A calm cold red planet covered in dust"};
     int Icons[] = {R.mipmap.ic_earth, R.mipmap.ic_venus, R.mipmap.ic_mars};
 
+    AccessPlanets access = new AccessPlanets();
+    List<Location> planetList = access.getPlanets();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planet_list);
 
+        String planetName[] = new String[planetList.size()];
+        String planetDescription[] = new String[planetList.size()];
+        int planetIcon[] = new int[planetList.size()];
+        for(int i = 0; i < planetList.size(); i++){
+            planetName[i] = planetList.get(i).getId();
+            planetDescription[i] = planetList.get(i).getDescription();
+            planetIcon[i] = Integer.parseInt(planetList.get(i).getImgSrc());
+        }
+
         simpleList = (ListView)findViewById(R.id.ListView); //makes listview based on info fed in
-        CustomAdapter customAdapter = new CustomAdapter(PlanetList.this, Planet, Description, Icons);
+        CustomAdapter customAdapter = new CustomAdapter(PlanetList.this, planetName, planetDescription, planetIcon);
         simpleList.setAdapter(customAdapter);
     }
 }
