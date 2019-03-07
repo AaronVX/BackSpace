94c602fe1a83e2eec769efb83ebd2c26f0586d3c
Xiaojian Xie <xiaojianxie@Xiaojians-Air.hitronhub.home>
2019-03-07 17:57:54 -0600

    fix constant issue

--- app/src/main/java/com/group8/backspace/logic/CalculatePrice.java
+++ app/src/main/java/com/group8/backspace/logic/CalculatePrice.java
@@ -5,10 +5,10 @@ import com.group8.backspace.objects.Location;
 public class CalculatePrice {
     private int ticketPrice;
     private int classPrice;
-
+    private int INDEX = 10000;
     public CalculatePrice(int flightNum, int classPrice){
 
-        AccessFlights accessor =   new AccessFlights();
+        AccessFlights accessor = new AccessFlights();
         Flight currFlight = accessor.getFlightByID(flightNum);
         String origin = currFlight.getOrigin();
         String destination = currFlight.getDestination();
@@ -19,7 +19,7 @@ public class CalculatePrice {
 
         double distance = Math.abs(Double.parseDouble(originPlanet.getDistance()) - Double.parseDouble(destinationPlanet.getDistance()));
 
-        ticketPrice = (int)distance*10000;
+        ticketPrice = (int)(distance*INDEX);
 
         this.classPrice = classPrice;
     }
@@ -31,9 +31,5 @@ public class CalculatePrice {
 
     public int getTotalPrice(){return  ticketPrice + classPrice;}
 
-    public void applyCoupon()
-    {
-        ticketPrice *= 0.9;
-    }
 
 }
