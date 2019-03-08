e6129df7c894eda90b1018b27989a4895bfe6c7c
Xiaojian Xie <xiaojianxie@Xiaojians-Air.hitronhub.home>
2019-03-07 18:43:22 -0600

    remove unused variable in CheckCard

--- app/src/main/java/com/group8/backspace/logic/CheckCard.java
+++ app/src/main/java/com/group8/backspace/logic/CheckCard.java
@@ -1,15 +1,6 @@
 package com.group8.backspace.logic;
 
 public class CheckCard {
-    private String cardNum;
-    private String date;
-    private String securityNum;
-
-    public CheckCard(String cardNum, String date, String securityNum){
-        this.cardNum = cardNum;
-        this.date = date;
-        this.securityNum = securityNum;
-    }
 
     public boolean checkFormat(String cardNum, String date, String securityNum){
         if(cardNum.length()==16&&date.length()==4&&securityNum.length()==3){
