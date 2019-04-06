c3ba2e5f2ed4aa8e9df94f09454e4f18863be5b9
unknown <lenabatb@myumanitoba.ca>
2019-04-06 18:22:23 -0500

    seperate logic in select origin

---
+++ app/src/main/java/com/group8/backspace/logic/CheckPlanetList.java
@@ -0,0 +1,39 @@
+package com.group8.backspace.logic;
+
+import com.group8.backspace.application.Services;
+import com.group8.backspace.logic.accessors.AccessPlanets;
+import com.group8.backspace.logic.exceptions.CouponException;
+import com.group8.backspace.objects.Location;
+
+import java.util.List;
+
+public class CheckPlanetList {
+
+    String destination;
+
+    public CheckPlanetList(){
+        destination = "";
+    }
+
+    public void setDestination(String origin) {
+        this.destination = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public boolean hasOrigin() { //null value not possible by this method call
+        boolean toReturn = false;
+        AccessPlanets access = new AccessPlanets(Services.getPlanetPersistence());
+        List<Location> planetList = access.getPlanets();
+
+        for (int i = 0; i < planetList.size(); i++) {
+            if (planetList.get(i).equals(destination)){
+                toReturn = true;
+            }
+        }
+
+        return toReturn;
+    }
+}
